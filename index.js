82e38c553da6377e1443e1c806caba5b926d8117
Gabriel Gambarra <gambarragabriel@gmail.com>
2019-11-14 00:18:40 -0400

    Update layout

---
+++ index.js
@@ -0,0 +1,32 @@
+function openGithubLink(repo) {
+    switch (repo) {
+        case 'instarocket':
+            window.open("https://github.com/gabrielgambarra/semana-omnistack7");
+            break;
+
+        default:
+            break;
+    }
+}
+
+function openGithubPage(repo) {
+    switch (repo) {
+        case 'instarocket':
+            window.open("https://github.com/gabrielgambarra/semana-omnistack7");
+            break;
+
+        default:
+            break;
+    }
+}
+
+function openLinkToAPK(repo) {
+    switch (repo) {
+        case 'instarocket':
+            window.open("https://github.com/gabrielgambarra/semana-omnistack7");
+            break;
+
+        default:
+            break;
+    }
+}
