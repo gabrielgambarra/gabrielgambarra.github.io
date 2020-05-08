cbb68c9883fa1fe621c67efcf83b6ff9dca49356
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-05-08 03:30:17 -0400

    Refatoração do layout do portfólio

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+window.alert("This site is being built. Many other things will be available as soon as possible.")
+
+const hamburguer = document.querySelector('.hamburguer');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
+});
+
 function openGithubLink(repo) {
     switch (repo) {
         case 'instarocket':
